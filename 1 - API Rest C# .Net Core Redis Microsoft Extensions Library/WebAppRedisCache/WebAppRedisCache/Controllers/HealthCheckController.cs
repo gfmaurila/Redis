9da075f96414e261e5f331d34da9faaf11a228be
Guilherme F Maurila <gfmurila@gmail.com>
2022-01-11 00:03:57 -0300

    1 - API Rest C# .Net Core Redis Microsoft Extensions Library

---
+++ 1 - API Rest C# .Net Core Redis Microsoft Extensions Library/WebAppRedisCache/WebAppRedisCache/Controllers/HealthCheckController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+
+namespace WebAppRedisCache.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        private readonly IDistributedCache _distributedCache;
+        public HealthCheckController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        [HttpGet]
+        public string GetStatus()
+        {
+            var key = "healthCheckStatus";
+            //var lastStatus = _distributedCache.Get(key);
+            var lastStatus = _distributedCache.GetString(key);
+            var actualStatus = $"{DateTime.UtcNow:o}";
+            _distributedCache.SetString(key, actualStatus);
+
+            var result = $"Atual Status: {actualStatus} | Antigo Status: {lastStatus}";
+
+            return result;
+        }
+    }
+}
