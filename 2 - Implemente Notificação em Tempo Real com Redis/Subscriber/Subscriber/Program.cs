11327e01dbea707dd3d8d37aaf9538c8881dd526
Guilherme F Maurila <gfmurila@gmail.com>
2022-01-11 22:11:33 -0300

    2 - Implemente Notificação em Tempo Real com Redis

---
+++ 2 - Implemente Notificação em Tempo Real com Redis/Subscriber/Subscriber/Program.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+            var subscriber = connection.GetSubscriber();
+
+            subscriber.Subscribe("order", (channel, message) =>
+            {
+                Console.WriteLine($"{DateTime.UtcNow:o} => {message}");
+            }, CommandFlags.None);
+
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # C# Redis
 - 1 - API Rest C# .Net Core Redis Microsoft Extensions Library
+- 2 - Implemente Notificação em Tempo Real com Redis
 
