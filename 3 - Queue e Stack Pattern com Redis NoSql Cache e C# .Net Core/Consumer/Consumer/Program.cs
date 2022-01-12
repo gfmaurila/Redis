a403b549209e1a128bbefd4811ff1d3d9c23e49a
Guilherme F Maurila <gfmurila@gmail.com>
2022-01-11 22:12:20 -0300

    3 - Queue e Stack Pattern com Redis NoSql Cache e C# .Net Core

---
+++ 3 - Queue e Stack Pattern com Redis NoSql Cache e C# .Net Core/Consumer/Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Threading;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conn = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+
+            var database = conn.GetDatabase();
+
+            var key = "1212121";
+
+            while (true)
+            {
+                var data = database.ListLeftPop(key);
+
+                Console.WriteLine($"{DateTime.UtcNow:o} => {data}");
+                Thread.Sleep(100);
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # C# Redis
 - 1 - API Rest C# .Net Core Redis Microsoft Extensions Library
 - 2 - Implemente Notificação em Tempo Real com Redis
+- 3 - Queue e Stack Pattern com Redis NoSql Cache e C# .Net Core
 
